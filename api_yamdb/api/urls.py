afa72de07dc375001f4fd35741209fd7a55dbcbf
Александр Трубников <petrovskii1980@mail.ru>
2023-05-17 20:16:15 +0300

    попытка настроить локальную работу

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,21 +1,37 @@
 from rest_framework import routers
 from django.urls import include, path
 
-from .views import ReviewViewSet, CommentViewSet, CategoryAPIView
+from .views import (
+    ReviewViewSet,
+    CommentViewSet,
+    CategoryAPIView,
+    UserViewSet,
+    TitleAPIView,
+    GenreAPIView,
+    send_confirmation_code,
+    send_token_jwt,
+)
 
 app_name = 'api'
 
 router_v1 = routers.DefaultRouter()
 
 router_v1.register('reviews', ReviewViewSet, basename='review')
+router_v1.register('titles', TitleAPIView, basename='titles')
+router_v1.register('genres', GenreAPIView, basename='genres')
+router_v1.register('categories', CategoryAPIView, basename='categories')
 router_v1.register(r'titles/(?P<title_id>\d+)/reviews/', ReviewViewSet,
                    basename='reviews')
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)comments/',
     CommentViewSet, basename='comments')
+router_v1.register('users', UserViewSet)
+router_v1.register('users/me', UserViewSet)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
-    path('v1/categories/', CategoryAPIView.as_view(),
-         name='category-list-create-delete'),
+    path('v1/auth/signup/', send_confirmation_code),
+    path('v1/auth/token/', send_token_jwt),
+    # path('v1/categories/', CategoryAPIView.as_view(),
+    #    name='category-list-create-delete'),
 ]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,14 +1,73 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import generics, permissions, viewsets
-from reviews.models import Category, Review, Title
-from .serializers import CategorySerializer, ReviewSerializer, CommentSerializer
+from rest_framework import generics, permissions, viewsets, filters, status
+from reviews.models import Category, Review, Title, User
+from .serializers import (
+    CategorySerializer,
+    ReviewSerializer,
+    CommentSerializer,
+    SignupUserSerializer,
+    TokenUserSerializer,
+    UserSerializer,
+)
 
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (
+    IsAuthenticatedOrReadOnly,
+    AllowAny,
+    IsAuthenticated,
+)
 from rest_framework.pagination import PageNumberPagination
-from .permissions import IsAuthorOrModerOrAdmin
+from .permissions import IsAuthorOrModerOrAdmin, AdminPermissions
+
+from rest_framework.decorators import action, api_view, permission_classes
+
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import AccessToken
+
+from api.util import (
+    confirmation_code_generation,
+    send_confirmation_code_to_email,
+)
 
 
-class CategoryAPIView(generics.ListCreateDestroyAPIView):
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'username'
+    permission_classes = (IsAuthenticated, AdminPermissions)
+    pagination_class = PageNumberPagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('username', )
+
+    # @action(
+    #     methods=['get', 'patch'],
+    #     detail=True,
+    #     url_path='me',
+    #     serializer_class=UserSerializer,
+    #     permission_classes=[IsAuthorOrModerOrAdmin, ],
+    # )
+    # def users_me(self, request, pk=None):
+    #     user = request.user
+    #     if request.method == 'GET':
+    #         serializer = self.get_serializer(
+    #             user,
+    #             data=request.data,
+    #             partial=True
+    #         )
+    #         serializer.is_valid(raise_exception=True)
+    #         serializer.save()
+    #         return Response(
+    #             serializer.data,
+    #             status=status.HTTP_200_OK,
+    #         )
+    #     if request.method == 'PATCH':
+    #         serializer = self.get_serializer(user)
+    #         return Response(
+    #             serializer.data,
+    #             status=status.HTTP_200_OK,
+    #         )
+
+
+class CategoryAPIView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
@@ -62,3 +121,75 @@ class CommentViewSet(viewsets.ModelViewSet):
         )
         serializer.save(author=self.request.user, review=review)
 
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def send_confirmation_code(request):
+    """
+    Обработка данных пользователя поступивших с api/v1/auth/signup/
+    и отправка confirmation_code на email пользователя.
+    """
+    if User.objects.filter(username=request.data.get('username')).exists():
+        if User.objects.get(
+            username=request.data.get('username')
+        ).email != request.data.get('email'):
+            return Response(
+                request.data,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        user = User.objects.get(username=request.data.get('username'))
+        user.confirmation_code = confirmation_code_generation()
+        user.save(update_fields=['confirmation_code'])
+        send_confirmation_code_to_email(user.confirmation_code, user.email)
+
+        return Response(
+            request.data,
+            status=status.HTTP_200_OK,
+        )
+    serializer = SignupUserSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        confirmation_code = confirmation_code_generation()
+        user = User.objects.create(
+            username=serializer.validated_data.get('username'),
+            email=serializer.validated_data.get('email'),
+            confirmation_code=confirmation_code
+        )
+        send_confirmation_code_to_email(user.confirmation_code, user.email)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK,
+        )
+    return Response(
+        serializer.error_messages,
+        status=status.HTTP_400_BAD_REQUEST,
+    )
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def send_token_jwt(request):
+    """
+    Обработка данных поступивших с api/v1/auth/token/
+    и возвращение в ответ на запрос access токен.
+    """
+    serializer = TokenUserSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        username = serializer.validated_data.get('username')
+        confirmation_code = serializer.validated_data.get('confirmation_code')
+        user = get_object_or_404(User, username=username)
+        if user.confirmation_code == confirmation_code:
+            access_jwt_token = AccessToken.for_user(user)
+            return Response(
+                f'token: {access_jwt_token}',
+                status=status.HTTP_200_OK,
+            )
+        return Response(
+            'Введен не верный confirmation_code.',
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+    return Response(
+        serializer.error_messages,
+        status=status.HTTP_400_BAD_REQUEST,
+    )
