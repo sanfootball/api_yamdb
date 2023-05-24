30e6c4b9ca3beb0d15316ed43aaa202875de5e6d
Александр Трубников <petrovskii1980@mail.ru>
2023-05-24 22:44:22 +0300

    Работа над замечаниями ревьюера

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -2,34 +2,86 @@ from django.contrib import admin
 
 from .models import User, Category, Genre, Title, Review, Comment
 
-admin.site.register(User)
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Title)
-admin.site.register(Review)
-admin.site.register(Comment)
-
-
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'text', 'pub_date', 'author')
-    search_fields = ('text',)
-    list_filter = ('pub_date',)
-    empty_value_display = '-пусто-'
+# admin.site.register(User)
+# admin.site.register(Category)
+# admin.site.register(Genre)
+# admin.site.register(Title)
+# admin.site.register(Review)
+# admin.site.register(Comment)
 
 
 class BaseAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-# @admin.register(User)
-# class UserAdmin(BaseAdmin):
-#     list_display = (
-#         'username',
-#         'email',
-#         'first_name',
-#         'last_name',
-#         'bio',
-#         'role',
-#     )
-#     search_fields = ('username',)
-#     list_filter = ('username',)
+@admin.register(User)
+class UserAdmin(BaseAdmin):
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'bio',
+        'role',
+    )
+    search_fields = ('username',)
+    list_filter = ('username',)
+
+
+@admin.register(Category)
+class CategoryAdmin(BaseAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+@admin.register(Genre)
+class GenreAdmin(BaseAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+@admin.register(Title)
+class TitleAdmin(BaseAdmin):
+    list_display = (
+        'name',
+        'year',
+        'description',
+        'category',
+    )
+
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+@admin.register(Review)
+class ReviewAdmin(BaseAdmin):
+    list_display = (
+        'title',
+        'text',
+        'author',
+        'score',
+        'pub_date',
+    )
+
+    search_fields = ('title',)
+    list_filter = ('title',)
+
+
+@admin.register(Comment)
+class CommentAdmin(BaseAdmin):
+    list_display = (
+        'review',
+        'text',
+        'author',
+        'pub_date',
+    )
+    search_fields = ('author',)
+    list_filter = ('author',)
